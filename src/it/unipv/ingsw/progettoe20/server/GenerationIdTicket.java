010ebc4ee539d40f8c1b21972f94c11096ac7f80
giulia coraglia <giulia.coraglia01@universitadipavia.it>
2020-05-19 20:38:03 +0200

    Modifica paramentri GenerateId

--- src/it/unipv/ingsw/progettoe20/server/GenerationIdTicket.java
+++ src/it/unipv/ingsw/progettoe20/server/GenerationIdTicket.java
@@ -7,9 +7,8 @@ public class GenerationIdTicket {
 			"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "z", "y",
 			"j", "k", "x", "w", "A", "B", "C", "D", "E", "F", "G", "H", "I",
 			"L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "Z", "Y",
-			"J", "K", "X", "W", "à", "è", "é", "ì", "ò", "ù" };
+			"J", "K", "X", "W"};
 	private String[] numCharacters = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
-	private String[] specialCharacters = { "!", "£", "$", "%", "&", "@", "*"};
 	// creazione oggetto per la generazione casuale
 	private Random rand = new Random();
 
@@ -18,7 +17,6 @@ public class GenerationIdTicket {
 		// ottengo la lunghezza di ogni array
 		int lungCaratteri = alfaCharacters.length;
 		int lungNumeri = numCharacters.length;
-		int lungSpeciali= specialCharacters.length;
 		String randomString = "A";				
 		
 		while (randomString.length() < lungId) {
@@ -26,11 +24,10 @@ public class GenerationIdTicket {
 			// ottengo un elemento casuale per ogni array
 			int c = rand.nextInt(lungCaratteri);
 			int n = rand.nextInt(lungNumeri);
-			int s = rand.nextInt(lungSpeciali);
 			// aggiungo una lettera casuale
 			randomString += alfaCharacters[c];
 			randomString += numCharacters[n];
-			randomString += specialCharacters[s];			
+					
 		}
 		// se la stringa generata dovesse superare il numero di caratteri
 		// richiesto, la taglio.
